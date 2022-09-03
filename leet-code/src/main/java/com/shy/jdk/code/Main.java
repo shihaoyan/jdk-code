d96f9ce1c459ee45b78a246275d93f1006598d1b
shihaoyan <shihaoyan0209@163.com>
2022-09-03 22:05:37 +0800

    feat: 算法

--- leet-code/src/main/java/com/shy/jdk/code/Main.java
+++ leet-code/src/main/java/com/shy/jdk/code/Main.java
@@ -1,5 +1,7 @@
 package com.shy.jdk.code;
 
+import java.util.Scanner;
+
 /**
  * @author shihaoyan
  * @date 2022/9/1 22:04
@@ -7,6 +9,20 @@ package com.shy.jdk.code;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int beichushu = sc.nextInt();
+        int chushu = sc.nextInt();
+        int num = test01(beichushu, chushu);
+        System.out.println(num);
+    }
 
+    public static int test01(int beichushu, int chushu) {
+        int temp = beichushu;
+        int num = 0;
+        while (temp >= chushu) {
+            num++;
+            temp -= chushu;
+        }
+        return num;
     }
 }
